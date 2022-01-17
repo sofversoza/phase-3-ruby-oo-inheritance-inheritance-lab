954f4d2025f07eb388960a0c20dfb13a8896106d
sofiaversoza <sofversoza@gmail.com>
2022-01-17 14:21:25 -0700

    commit tree f12ac2bfdb57a1e6596a1b1294ce9a1e08ba9510

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+    attr_accessor :knowledge
 
+    def initialize
+        @knowledge = []
+    end
+
+    def learn(new_knowledge)
+        @knowledge << new_knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
 end
